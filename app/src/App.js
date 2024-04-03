ae5798d12c13606d2c607993f469a099541e9170
Somanshu Rath <somanshurath.dev@gmail.com>
2024-04-03 20:54:17 +0530

    Kalyan w.u.p

--- app/src/App.js
+++ app/src/App.js
@@ -3,7 +3,7 @@ import { MapContainer, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import { GeoJSON } from 'react-leaflet';
 import { DombivliRoads } from './roads/DombivliRoads.js';
-import { newRoads } from './roads/newRoads.js';
+import { KalyanRoads_Bank, KalyanRoads_KalWest } from './roads/KalyanRoads.js';
 
 function App() {
     return (
@@ -11,8 +11,8 @@ function App() {
             <h2 className='heading'>Flood Mapping and Prediction</h2>
             <div className='map' id='map'>
                 <MapContainer
-                    center={[19.22946599675518, 73.0800348686054]}
-                    zoom={15}
+                    center={[19.239022963415673, 73.10866168990492]}
+                    zoom={14}
                 >
                     <TileLayer
                         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
@@ -24,19 +24,30 @@ function App() {
                             key={index}
                             data={road}
                             style={() => ({
-                                color: 'red',
+                                color: 'blue',
                                 weight: 2,
                                 opacity: 0.6,
                             })}
                         />
                     ))}
 
-                    {newRoads.features.map((road, index) => (
+                    {KalyanRoads_Bank.features.map((road, index) => (
                         <GeoJSON
                             key={index}
                             data={road}
                             style={() => ({
-                                color: 'blue',
+                                color: 'red',
+                                weight: 2,
+                                opacity: 0.6,
+                            })}
+                        />
+                    ))}
+                    {KalyanRoads_KalWest.features.map((road, index) => (
+                        <GeoJSON
+                            key={index}
+                            data={road}
+                            style={() => ({
+                                color: 'green',
                                 weight: 2,
                                 opacity: 0.6,
                             })}
