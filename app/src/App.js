03bdfa1d34923afadd2732e891c747ebc22e496f
Somanshu Rath <somanshurath.dev@gmail.com>
2024-04-06 00:22:28 +0530

    Added terrain view and increased area of coverage.

--- app/src/App.js
+++ app/src/App.js
@@ -3,32 +3,52 @@ import React, { useState } from 'react';
 import { MapContainer, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import { GeoJSON } from 'react-leaflet';
-import { DombivliBanks, DombivliCentral } from './roads/DombivliRoads.js';
+import {
+    DombivliBanks,
+    DombivliCentral,
+    DombivliWestBanks,
+} from './roads/DombivliRoads.js';
 import { KalyanRoads_Bank, KalyanRoads_KalWest } from './roads/KalyanRoads.js';
 import { DivaRoads, LowerMumbra } from './roads/Mumbra-DivaRoads.js';
 
 function App() {
     const darktileUrl =
-        'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png/';
+        'https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png';
 
     const lighttileUrl = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
 
-    const [tileUrl, setTileUrl] = useState(lighttileUrl);
+    const satellitetileUrl =
+        'http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}';
+
+    const [tileUrl, setTileUrl] = useState(
+        localStorage.getItem('lastTileUrl') || lighttileUrl
+    );
 
     const switchTileProvider = () => {
+        let newTileUrl;
         if (tileUrl === darktileUrl) {
-            setTileUrl(lighttileUrl);
+            newTileUrl = lighttileUrl;
+        } else if (tileUrl === lighttileUrl) {
+            newTileUrl = satellitetileUrl;
         } else {
-            setTileUrl(darktileUrl);
+            newTileUrl = darktileUrl;
         }
+        setTileUrl(newTileUrl);
+        localStorage.setItem('lastTileUrl', newTileUrl);
     };
+
     return (
         <div className='App'>
             <h2 className='heading'>Flood Mapping and Prediction</h2>
             <button className='toggle' onClick={switchTileProvider}>
-                <i className='material-icons'>
-                    {tileUrl === darktileUrl ? 'nights_stay' : 'wb_sunny'}
-                </i>
+                <i className='material-icons' style={{ marginRight: '0.3em' }}>
+                    {tileUrl === darktileUrl
+                        ? 'nights_stay'
+                        : tileUrl === lighttileUrl
+                        ? 'wb_sunny'
+                        : 'public'}
+                </i>{' '}
+                Change View
             </button>
             <div className='map' id='map'>
                 <MapContainer
@@ -96,6 +116,17 @@ function App() {
                         />
                     ))}
                     {LowerMumbra.features.map((road, index) => (
+                        <GeoJSON
+                            key={index}
+                            data={road}
+                            style={() => ({
+                                color: 'red',
+                                weight: 2,
+                                opacity: 0.6,
+                            })}
+                        />
+                    ))}
+                    {DombivliWestBanks.features.map((road, index) => (
                         <GeoJSON
                             key={index}
                             data={road}
